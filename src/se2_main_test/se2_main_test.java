93db9be8f520edb364179921dd9b434058be10c2
PandengYuan <81248451+PandengYuan@users.noreply.github.com>
2021-05-21 04:09:24 +0100

    第六版 （由于单例模式，main方法做相应调整。其它没动）

---
+++ src/se2_main_test/se2_main_test.java
@@ -0,0 +1,35 @@
+package se2_main_test;
+
+
+import java.util.Scanner;
+
+import business.SystemInterface;
+import fileIO.DataManagement;
+import user.Administrator;
+import user.Director;
+
+
+public class se2_main_test {
+	
+	public static void main(String[] args) {
+		
+		String dataAddress = "C:\\file.txt";
+		
+		//initialize systemInterface with required input data
+		SystemInterface systemInterface =  SystemInterface.getInstance(new DataManagement().loadData(dataAddress), new Scanner(System.in));
+		
+		//initialize users
+		Director director = new Director(systemInterface);
+		Administrator administrator = new Administrator(systemInterface);
+		
+		//business execution
+		director.produceTeachingRequirements();
+		administrator.chooseSuitableTeachers();
+		administrator.organiseTrainingForTeachers();
+
+		//write updated data to file
+		new DataManagement().writeData(systemInterface.fileOutputProcess(), dataAddress);
+		
+	}
+
+}
